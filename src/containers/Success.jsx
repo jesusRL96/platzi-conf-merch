d3a27cc0fe6aa3166a15635ac5f1c7df47dbffc2
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-11-16 15:54:10 -0600

    Maquetación y estilos del flujo de pago

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Payment = () => {
-  return <h1>Payment</h1>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Juan, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu direccion</span>
+        <div className="Success-map">Google maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
