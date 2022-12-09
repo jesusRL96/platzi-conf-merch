9579ca68df69a739488f1ea16bb4637a33481059
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-12-09 15:30:13 -0600

    redirect

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 import '../styles/components/Information.css';
 
 const Information = () => {
   const { state, addToBuyer } = useContext(AppContext);
   const form = useRef(null);
+  const navigate = useNavigate();
   const { cart } = state;
 
   const handleSubmit = () => {
@@ -21,6 +22,7 @@ const Information = () => {
       phone: formData.get('phone'),
     };
     addToBuyer(buyer);
+    navigate('/checkout/payment');
   };
 
   return (
